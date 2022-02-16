05943b8b97486b7a00ada3689f82371475932a00
EdenGenetTasama <edentasama19@gmail.com>
2022-02-16 13:40:10 +0200

    add game component

---
+++ src/components/Game/game.tsx
@@ -0,0 +1,20 @@
+
+
+const Game=()=>{
+
+    let arrayNumber:number[];
+
+    const GetNumUntil0=(num:number)=>{
+            if(num != 0){
+                arrayNumber.push(num);
+            }
+
+    }
+
+
+    return(<div>
+        <input type="button" placeholder="Number"/>
+    </div>)
+}
+
+export default Game;
